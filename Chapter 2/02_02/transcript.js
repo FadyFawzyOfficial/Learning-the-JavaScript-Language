cef64d52318974563e09c7cfe2d21a2213a35f84
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-15 20:06:12 +0300

    2. Use Strings

---
+++ Chapter 2/02_02/transcript.js
@@ -0,0 +1,17 @@
+"This is a string";
+"12";
+12;
+
+'This is also a string';
+// 'This is a string"; // mismatched quotes - this will not execute
+
+'<a href="">';
+'<a href="http://www.fadyfawzy.com">';
+
+"This is Fady's favorite string";
+"This is Fady's \"favorite\" string";
+// "This is Joe's "favorite" string"; // this line will not work
+
+"This is \
+Fady's Favorite \
+String Ever";
