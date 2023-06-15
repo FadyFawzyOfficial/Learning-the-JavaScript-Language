a38f6954078de5557963d84c33e684ec51ba822f
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-15 20:10:33 +0300

    3. Explore String properties and methods

---
+++ Chapter 2/02_03/transcript.js
@@ -0,0 +1,18 @@
+var myString = "This is my string. Leave it alone";
+myString;
+console.log(myString);
+myString.length;
+console.log(myString.length);
+myString.toUpperCase();
+console.log(myString.toUpperCase());
+
+"This is my string".length;
+console.log("This is my string".length);
+
+let declaration = `This I say to you: "good morning". Fady!`;
+declaration;
+console.log(declaration);
+
+declaration = `This I say to you: "${myString}". Fady!`;
+declaration;
+console.log(declaration);
