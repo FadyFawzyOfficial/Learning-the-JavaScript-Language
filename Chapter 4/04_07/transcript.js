d6a0631a1b415690b7507c1b77b38c1522d3a3d1
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-16 11:34:29 +0300

    7. Use Ternary Operator instead of if and else statement

---
+++ Chapter 4/04_07/transcript.js
@@ -0,0 +1,10 @@
+var animal = "cat";
+// animal = 'dog';
+
+animal === "cat"
+  ? console.log("You will be a cat herder.")
+  : console.log("You will be a dog catcher.");
+
+var job = animal === "cat" ? "cat herder" : "dog catcher";
+
+var job = (animal === "cat") ? "cat herder" : "dog catcher";
