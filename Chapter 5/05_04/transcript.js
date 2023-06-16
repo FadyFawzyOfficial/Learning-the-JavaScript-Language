44b9ea6a34a9efb07ede3a2adb43478aa23a788f
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-16 12:07:59 +0300

    4. Explore While & Do While loops

---
+++ Chapter 5/05_04/transcript.js
@@ -0,0 +1,43 @@
+// for loop
+for (let i = 0; i < 10; i += 1) {
+  console.log(i);
+}
+
+// same thing as a while loop
+let i = 0;
+while (i < 10) {
+  console.log(i + "... This will go until we hit 10");
+  i += 1;
+}
+
+var myList = [true, true, true, false, true, true];
+
+var myItem = null;
+
+while (myItem !== false) {
+  console.log(
+    "myList has " +
+      myList.length +
+      " items now. This loop will keep going until we pop a false."
+  );
+  myItem = myList.pop();
+}
+
+var counter = 1;
+while (true) {
+  console.log(counter);
+  counter++;
+  break; // comment out this break statement to make this loop go forever and potentially lock up your web browser
+}
+
+var myList = [true, true, true, false, true, true];
+
+var myItem = false;
+do {
+  console.log(
+    "myList has " +
+      myList.length +
+      " items now. This loop will go until we pop a false."
+  );
+  myItem = myList.pop();
+} while (myItem !== false);
