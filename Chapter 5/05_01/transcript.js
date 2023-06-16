ffb42b44b47c7d043c324801e9cc9aea4baea664
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-16 11:51:09 +0300

    1. Use For Loops: Sequential

---
+++ Chapter 5/05_01/transcript.js
@@ -0,0 +1,41 @@
+for (let i = 0; i < 10; i += 1) {
+  console.log(i);
+}
+
+// very common use case: looping over an array.
+var pageNames = [
+  "Home",
+  "About Us",
+  "Contact Us",
+  "JavaScript Playground",
+  "News",
+  "Blog"
+];
+
+for (i = 0; i < pageNames.length; i += 1) {
+  if (document.title === pageNames[i]) {
+    console.log("We ARE here: " + pageNames[i]);
+  } else {
+    console.log("We are not here: " + pageNames[i]);
+  }
+}
+
+// don't repeat yourself:
+var pageNames = [
+  "Home",
+  "About Us",
+  "Contact Us",
+  "JavaScript Playground",
+  "News",
+  "Blog"
+];
+
+for (i = 0; i < pageNames.length; i += 1) {
+  var currentPageTitle = pageNames[i];
+
+  if (document.title === currentPageTitle) {
+    console.log("We ARE here: " + currentPageTitle);
+  } else {
+    console.log("We are not here: " + currentPageTitle);
+  }
+}
