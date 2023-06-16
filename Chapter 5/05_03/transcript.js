6f13569b0a3f0a642c02b7c1bae46de637d58b24
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-16 12:05:18 +0300

    3. Explore Set and Map: Newer loopable data

---
+++ Chapter 5/05_03/transcript.js
@@ -0,0 +1,55 @@
+// This array has two copies of its first item
+let myList = [1, 1, 2, 3, 5, 8, 13, "fibonacci"];
+
+// The same thing using the Set API
+let mySet = new Set();
+mySet.add(1);
+mySet.add(1); // this won't change mySet, since 1 is already in there
+mySet.add(2);
+mySet.add(3);
+// ... this gets tedious
+
+// An array can be turned into a set
+// If you want to quickly remove all duplicates from an array, this is a good tool!
+let mySet2 = new Set(myList);
+
+mySet2.has(3); // true
+mySet2.has(12); // false
+
+// For...of loop iteration works
+//! For...in loop causes error
+for (let item of mySet2) {
+  console.log('mySet contains', item);
+}
+
+// This object is a bird
+var bird = {
+  genus: "corvus",
+  species: "corvax",
+  commonName: "raven",
+};
+
+// Here is a map using the same structure
+var birdMap = new Map();
+birdMap.set("genus", "corvus");
+birdMap.set("species", "corvax");
+birdMap.set("commonName", "raven");
+
+birdMap.get("genus"); // 'corvus'
+
+birdMap.has("genus"); // true
+birdMap.has("corvus"); // false (keys only)
+
+//! for...of loops work on Maps, with key and value returned
+for (let value of birdMap) {
+  console.log(value);
+}
+
+birdMap.entries();
+console.log(birdMap.entries());
+
+let birdObject = Object.entries(bird);
+console.log(birdObject);
+
+birdMap2 = new Map(Object.entries(bird));
+console.log(birdMap2);
